f1959af255312154e7c5005e83789e287a9f928f
Koomawat <52982332+Koomawat@users.noreply.github.com>
2021-03-01 16:34:48 -0500

    mine placer

--- createboard.py
+++ createboard.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 def board(dim):
 
@@ -6,3 +7,27 @@ def board(dim):
 
     return gameboard
 
+def minePlacer(n, board):
+
+    boardLen = len(board)
+
+    for i in range(n):
+        
+        tupleX = random.randint(0, boardLen-1)
+        tupleY = random.randint(0, boardLen-1)
+
+        if (board[tupleX,tupleY] == 0):
+            # 9 represents a mine 
+            board[tupleX,tupleY] = 9
+        else:
+
+            while(board[tupleX,tupleY] != 0):
+
+                tupleX = random.randint(0, boardLen-1)
+                tupleY = random.randint(0, boardLen-1)
+
+                if (board[tupleX,tupleY] == 0):
+                    # 9 represents a mine 
+                    board[tupleX,tupleY] = 9
+
+    return board
--- minesweeper.py
+++ minesweeper.py
@@ -10,12 +10,22 @@ def main():
     #mines = int(input("Enter the number of mines: "))
 
     minesweeper = board(boardDimension)
+    #minesweeper[0,5] = 1
+   
+
+    mines = int(input("Enter the number of mines: "))
+
+    minesweeper = minePlacer(mines, minesweeper)
 
     plt.imshow(minesweeper)
-    plt.xticks(np.arange(0.5, len(minesweeper), 1), color = 'white')
-    plt.yticks(np.arange(0.5, len(minesweeper), 1), color = 'white')
-    plt.grid(linestyle='-', linewidth='1', color ='white')
+    #plt.pcolor(minesweeper, edgecolors ='k', linewidths=1)
+    #plt.xticks(np.arange(0.5, len(minesweeper), 1), color = 'white')
+    #plt.yticks(np.arange(0.5, len(minesweeper), 1), color = 'white')
+    #plt.grid(linestyle='-', linewidth='1', color ='white')
     plt.show()
+
+    #print()
+    #print(minesweeper)
 
 if __name__ == "__main__":
     main()
