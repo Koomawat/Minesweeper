6f645ae72b27251d510c8061b0d75f58646053c8
Eric <eric.chuong@gmail.com>
2021-03-20 23:46:32 -0400

    Update

--- minesweeper.py
+++ minesweeper.py
@@ -4,50 +4,44 @@ from advAgent import *
 
 def main():
 
+    # Initialize the board
     boardDimension = int(input("Enter the dimension of the board: "))
-
     if boardDimension <= 5:
         print("Board too small")
         exit()
-
     initial = board(boardDimension)
 
-    mines = int(input("Enter the number of mines: "))
-
-    
+    # Place the desired amount of mines and display an error when there are more mines than available cells
+    mines = int(input("Enter the number of mines: "))    
     if mines >= boardDimension * boardDimension:
         print("Mine limit exceeded")
         exit()
-
     minesweeper = minePlacer(mines, initial)
 
     # printBoard(minesweeper)
 
     minesweeperHints = hintsCalculator(minesweeper)
 
-    printBoard(minesweeperHints)
-    #printAdvBoard(minesweeperHints)
+    #printBoard(minesweeperHints)
+    printAdvBoard(minesweeperHints)
 
-    result, mineHits = search(minesweeperHints, boardDimension)
-    #result, mineHits = advSearch(minesweeperHints, boardDimension)
+    #result, mineHits = search(minesweeperHints, boardDimension)
+    result, mineHits = advSearch(minesweeperHints, boardDimension)
 
     print('result: ')
-    printBoard(result)
-    #printAdvBoard(result)
-
+    #printBoard(result)
+    printAdvBoard(result)
     print()
 
     # M,m = mineScan(result, boardDimension)
 
-    percent = (mines-mineHits) / mines
-    percent *= 100
-
+    # Calculate the % of mines found
+    percent = ((mines-mineHits) / mines)*100
     print("Found", percent, "percent of mines without hitting a mine")
 
-    # Need to calculate the final score represented by the total number of mines - the mines exploded by the player
+    # Need to print the final score represented by the total number of mines - the mines exploded by the player
     finalscore = mines - mineHits
     print("Final Score:", finalscore, " out of ", mines)
-
     print('End of program.')
 
 if __name__ == "__main__":
--- plotting.py
+++ plotting.py
@@ -1,16 +1,23 @@
 from createboard import *
 from agent import *
 from advAgent import *
+from matplotlib import pyplot as plt
+from matplotlib import colors
+from decimal import Decimal
 
 # x = mine density
 # y = average final score
+def main():
+    plotting()
+    print("End of the program.")
+
 
 def plotting():
 
     # Initialize the averages for the basic and advanced agents
-    basicAgentAvg, advAgentAvg = [], []
+    basicAgentAvgList, advAgentAvgList = [], []
     # Mine density testing
-    mineDensity = [x * 0.05 for x in range(1, 20)]
+    mineDensity = [x * 0.05 for x in range(1, 7)] #change to 20
 
 
     # Get the desired board size => can change it to just 1 fixed size if neeeded
@@ -20,9 +27,9 @@ def plotting():
         exit()
 
     # Do minesweeper runs for each density being tested
-    for x in range(1, 20):
+    for x in range(1, 7):
         # Get the amount of mines needed for the current density
-        mines = (boardDimension * boardDimension) * 0.5 * x
+        mines = (boardDimension * boardDimension) * 0.05 * x
         # if for some reason it exceeds the limit (which it shouldnt)
         if mines > boardDimension * boardDimension:
             print("Mine limit exceeded")
@@ -32,18 +39,49 @@ def plotting():
         boards = []
 
         # Create this many boards
-        for y in range(0, 10):
+        for y in range(0, 1): # change this back to 10 later
             # Create the board
             initial = board(boardDimension)
             # Add mines to the board
-            minesweeper = minePlacer(mines, initial)
+            minesweeper = minePlacer(int(mines), initial)
             # Add the hints to the board
             finalBoard = hintsCalculator(minesweeper)
             # Add the board to the list
             boards.append(finalBoard)
         
+        # the total score of the agent in the 10 runs
+        basicAgentScore, advAgentScore = 0, 0
         # Since we are calculating for the average final score => 
         # total up the finalScores and divide by the # of mines * the amount of boards made
+        # now for each maze, run the basic and advanced agents
+        for currBoard in boards:
+            # Basic agent
+            result, mineHits = search(finalBoard, boardDimension)
+            basicScore = mines - mineHits
+            basicAgentScore += basicScore
+            # Advanced agent
+            #result, mineHits = advSearch(finalBoard, boardDimension)
+            #advScore = mines - mineHits
+            #advAgentScore += advScore
+
+        #put averages to be plotted here
+        basicAgentScoreAvg = basicAgentScore/1 # change to divide by 10
+        basicAgentAvgList.append(basicAgentScoreAvg)
+        #advAgentScoreAvg = advAgentScore/10
+        #advAgentAvgList.append(advAgentScoreAvg)
+
+    #Begin plotting
+    plt.plot(mineDensity, basicAgentAvgList, label = "Basic Agent")
+    #plt.plot(mineDensity, advAgentAvgList, label = "Advanced Agent")
+
+    plt.xlabel('Mine Density')
+    plt.ylabel('Average Final Score')
+    plt.title(f"Basic and Advanced Agent Final Score Comparisons\n Board size: {boardDimension} x {boardDimension}")
+    plt.legend(loc = "upper right")
+    plt.show()            
+
+        
+
 
 
 
